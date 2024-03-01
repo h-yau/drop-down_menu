072e222575bb46f7554e1398410d76b751f3ede8
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-03-01 00:22:12 -0800

    Enhance logic on mouseover and out

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const drpMenuListener = () => {
   const drpMenu = document.querySelector('.drp-menu');
-  drpMenu.addEventListener('click', () => {
+  drpMenu.addEventListener('mouseover', () => {
+    drpMenu.classList.toggle('visible');
+  });
+  drpMenu.addEventListener('mouseout', () => {
     drpMenu.classList.toggle('visible');
   });
 };
